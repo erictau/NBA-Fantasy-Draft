b0b5e9a16fbf328ff5242b1ce02f69b3fedc6b78
erictau <erictau04@gmail.com>
2022-08-10 15:28:15 -0700

    Applies Yeti Bootstrap theme from Bootswatch

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './pages/App/App';
 import { BrowserRouter as Router } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './bootstrap.min.css'
 
 
 
