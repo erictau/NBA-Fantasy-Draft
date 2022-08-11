9c9264b484f0c71b42d6b6440a876c0319d4c6c1
erictau <erictau04@gmail.com>
2022-08-11 15:19:51 -0700

    Adds a summary card to the PicksList component

--- src/components/PicksList/PicksList.jsx
+++ src/components/PicksList/PicksList.jsx
@@ -1,10 +1,41 @@
-import PlayerCard from '../PlayerCard/PlayerCard'
-export default function PicksList({ draftPicks }) {
+import { useState, useEffect } from 'react';
 
+import PlayerCard from '../PlayerCard/PlayerCard'
+import Card from 'react-bootstrap/Card'
+
+
+export default function PicksList({ draftPicks }) {
+    // State
+    const [positions, setPositions] = useState({'G': 0, 'F': 0, 'C': 0})
+    
+    // Effects
+    useEffect(function() {
+        let tempPositions = {'G': 0, 'F': 0, 'C': 0}
+        draftPicks.forEach(player => {
+            player.position.split('-').forEach(pos => {
+                tempPositions[pos] += 1
+            })
+        })
+        setPositions(tempPositions)
+    } ,[draftPicks])
 
     return (
         <div className="d-flex row align-items-center">
             <div className="container col-8">
+                <Card>
+                    <Card.Title>Summary Card</Card.Title>
+                    <Card.Body>
+                        <Card.Text>
+                            <div>
+                                Total Score: {draftPicks.reduce((total, player) => total += player.projectedScore, 0).toFixed(2)}
+                            </div>
+                            <div>
+                                Positions: G-{positions.G}, F-{positions.F}, C-{positions.C}
+                            </div>
+                        </Card.Text>
+                    </Card.Body>
+
+                </Card>
                 {draftPicks.map(player => <PlayerCard player={player} />)}
             </div>
         </div>
